fb284303b19e4b2647154fbf63989c93cf175ae5
Stephen Lane <lane.s@unimelb.edu.au>
2017-05-29 20:01:45 +1000

    Add some munging

--- R/data-prep.R
+++ R/data-prep.R
@@ -4,7 +4,7 @@
 ## Author: Steve Lane
 ## Date: Saturday, 27 May 2017
 ## Synopsis: Pull down data and transform into table.
-## Time-stamp: <2017-05-28 17:23:35 (steve)>
+## Time-stamp: <2017-05-29 20:01:32 (steve)>
 ################################################################################
 ################################################################################
 library(dplyr)
@@ -25,14 +25,46 @@ scores <- scores %>%
 scores <- scores %>%
     mutate(
         homeWin = ifelse(homeScore > awayScore, 1, 0),
-        awayWin = 1 - homeWin,
+        awayWin = ifelse(homeScore < awayScore, 1, 0),
         homeDraw = ifelse(homeScore == awayScore, 1, 0),
-        awayDraw = 1 - awayDraw,
+        awayDraw = ifelse(homeScore == awayScore, 1, 0),
         homeLoss = ifelse(homeScore < awayScore, 1, 0),
-        awayLoss = 1 - awayLoss,
+        awayLoss = ifelse(homeScore > awayScore, 1, 0),
         homePoints = case_when(
             .$homeScore > .$awayScore ~ 2,
             .$homeScore < .$awayScore ~ 0,
             .$homeScore == .$awayScore ~ 1),
         awayPoints = 2 - homePoints
     )
+
+## Now in long format for ease of manipulation
+scoresHome <- scores %>%
+    select(-contains("away"), -num_range("homeQ", 1:4)) %>%
+    rename(`Team Name` = `Home team`,
+           Win = homeWin,
+           Draw = homeDraw,
+           Loss = homeLoss,
+           Points = homePoints
+           )
+scoresAway <- scores %>%
+    select(-contains("home"), -num_range("awayQ", 1:4)) %>%
+    rename(`Team Name` = `Away team`,
+           Win = awayWin,
+           Draw = awayDraw,
+           Loss = awayLoss,
+           Points = awayPoints
+           )
+scoresLong <- bind_rows(scoresHome, scoresAway) %>%
+    group_by(`Team Name`) %>% arrange(`Round Number`) %>%
+    mutate(`Cumulative Points` = cumsum(Points),
+           For = cumsum(homeScore),
+           Against = cumsum(awayScore),
+           Percentage = For / Against
+           )
+
+
+scoresLong %>% group_by(`Team Name`) %>%
+    arrange(`Round Number`) %>%
+    mutate(cumPoints = cumsum(Points)) %>%
+    select(`Team Name`, Points, cumPoints) %>%
+    print.data.frame()
