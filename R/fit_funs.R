a2c87de3b437d355b0a67878db6eebfdfdc2e48d
Steve Lane <lane.s@unimelb.edu.au>
2018-04-22 13:25:17 +1000

    functions for helping fit data

---
+++ R/fit_funs.R
@@ -0,0 +1,37 @@
+################################################################################
+################################################################################
+## Title: Functions to fit models and produce output
+## Author: Steve Lane
+## Date: Sunday, 22 April 2018
+## Synopsis: Description
+## Time-stamp: <2018-04-22 13:20:41 (slane)>
+################################################################################
+################################################################################
+
+## Function takes stan output and produces a histogram and results table
+predDiffHist <- function(game, model, game_lookup) {
+    home <- toupper(game_lookup$homeTeam[game])
+    away <- toupper(game_lookup$awayTeam[game])
+    pred_diff <- extract(model, paste0("pred_diff[", game, "]"))[[1]]
+    dens <- density(pred_diff)
+    pred_df <- data_frame(score_diff = dens$x, y = dens$y) %>%
+        mutate(prob_win = ifelse(score_diff > 0, home, away))
+    col_list = c(game_lookup$homeColour[game],
+                 game_lookup$awayColour[game])
+    names(col_list) <- c(home, away)
+    prob <- extract(model, paste0("prob_home[", game, "]"))[[1]]
+    prob <- round(mean(prob) * 100)
+    title_text <- paste0(home, " v.s. ", away)
+    subtitle_text <- paste0("Chance of ", home, " winning: ", prob, "%")
+    pl_diff <- ggplot(pred_df, aes(x = score_diff, y = y, fill = prob_win)) +
+        geom_line() +
+        geom_ribbon(aes(ymin = 0, ymax = y, fill = prob_win)) +
+        scale_fill_manual(values = col_list, name = NULL) +
+        xlab("Score differential") +
+        ylab("") +
+        ggtitle(title_text, subtitle_text)
+    df <- game_lookup %>%
+        slice(game) %>%
+        mutate(prob = paste0(prob, "%"))
+    list(plot = pl_diff, results = df)
+}
